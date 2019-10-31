8614120542a90cecc9e1db33608aa0198a626801
PolarToCartesian <hleberre64@gmail.com>
2019-10-31 01:58:18 +0100

    Commit #2

--- Window.h
+++ Window.h
@@ -7,6 +7,7 @@ enum class Event
     WINDOW_CLOSED,
     WINDOW_MOVED,
     CURSOR_MOVED,
+    KEY_DOWN,
     MOUSE_WHEEL_ROTATING,
     LEFT_MOUSE_BUTTON_CLICKED,
     RIGHT_MOUSE_BUTTON_CLICKED,
@@ -22,12 +23,201 @@ enum class Event
 #include <cstdint>
 #include <iostream>
 #include <algorithm>
+#include <functional>
 
 #include <Windows.h>
 #include <Windowsx.h>
 
 LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lparam);
 
+class Window;
+
+class Mouse
+{
+    friend Window;
+
+    private:
+        Vec2u m_position{0, 0};
+        Vec2u m_deltaPosition{0, 0};
+        
+        int16_t m_wheelDelta = 0;
+
+        bool m_isLeftButtonDown  = false;
+        bool m_isRightButtonDown = false;
+
+        std::function<void(const Vec2u)> m_onLeftClickFunctor   = [](const Vec2u pos) {};
+        std::function<void(const Vec2u)> m_onRightClickFunctor  = [](const Vec2u pos) {};
+        std::function<void(const int16_t)> m_onWheelTurnFunctor = [](const int16_t delta) {};
+
+    public:
+        Mouse() {}
+
+        void onLeftClick (const std::function<void(Vec2u)>& functor)         { this->m_onLeftClickFunctor  = functor; }
+        void onRightClick(const std::function<void(Vec2u)>& functor)         { this->m_onRightClickFunctor = functor; }
+        void onWheelTurn (const std::function<void(const int16_t)>& functor) { this->m_onWheelTurnFunctor  = functor; }
+
+        void onUpdate()
+        {
+            this->m_wheelDelta = 0;
+            this->m_deltaPosition = {0, 0};
+        }
+
+        bool handleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
+        {
+            switch (msg)
+            {
+                case WM_MOUSEMOVE:
+                    {
+                        this->m_deltaPosition = {
+                            static_cast<int16_t>(static_cast<int16_t>(GET_X_LPARAM(lParam)) - static_cast<int16_t>(this->m_position[0])),
+                            static_cast<int16_t>(static_cast<int16_t>(GET_Y_LPARAM(lParam)) - static_cast<int16_t>(this->m_position[1]))
+                        };
+
+                        this->m_position = {(uint16_t)GET_X_LPARAM(lParam), (uint16_t)GET_Y_LPARAM(lParam)};
+                    }
+                    return true;
+                case WM_LBUTTONDOWN:
+                    this->m_isLeftButtonDown  = true;
+                    return true;
+                case WM_LBUTTONUP:
+                    this->m_isLeftButtonDown  = false;
+                    this->m_onLeftClickFunctor(this->m_position);
+                    return true;
+                case WM_RBUTTONDOWN:
+                    this->m_isRightButtonDown = true;
+                    return true;
+                case WM_RBUTTONUP:
+                    this->m_isRightButtonDown = false;
+                    this->m_onRightClickFunctor(this->m_position);
+                    return true;
+                case WM_MOUSEWHEEL:
+                    this->m_wheelDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+                    this->m_onWheelTurnFunctor(this->m_wheelDelta);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+};
+
+class Keyboard
+{
+    private:
+        std::vector<uint8_t> m_downKeys;
+
+    public:
+        Keyboard() {}
+
+        bool isKeyDown(const uint8_t key) { return this->m_downKeys.end() != std::find(this->m_downKeys.begin(), this->m_downKeys.end(), key); }
+
+        bool handleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
+        {
+            switch (msg)
+            {
+                case WM_KEYDOWN:
+                    {
+                        const uint8_t keyCode = static_cast<uint8_t>(wParam);
+
+                        if (std::find(this->m_downKeys.begin(), this->m_downKeys.end(), keyCode) == this->m_downKeys.end())
+                            this->m_downKeys.push_back(keyCode);
+
+                        return true;
+                    }
+                case WM_KEYUP:
+                    {
+                        const uint8_t keyCode = static_cast<uint8_t>(wParam);
+
+                        if (std::find(this->m_downKeys.begin(), this->m_downKeys.end(), keyCode) != this->m_downKeys.end())
+                            this->m_downKeys.erase(std::remove(this->m_downKeys.begin(), this->m_downKeys.end(), keyCode), this->m_downKeys.end());
+
+                        return true;
+                    }
+            }
+
+            return false;
+        }
+};
+
+class Window
+{
+    friend LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lparam);
+
+    private:
+        HWND m_handle;
+
+        Vec2u m_position{0, 0};
+        Vec2u m_dimension{0, 0};
+    
+        bool m_isRunning = false;
+
+    public:
+        Mouse m_mouse;
+        Keyboard m_keyboard;
+
+    public:
+        Window(const uint16_t width, const uint16_t height, const char *title, HINSTANCE hInstance)
+        {
+            this->m_dimension = { width, height };
+
+            const WNDCLASS wc{CS_HREDRAW | CS_VREDRAW, WindowProcessMessages, 0, 0, hInstance, NULL, LoadCursor(nullptr, IDC_ARROW), (HBRUSH)COLOR_WINDOW, NULL, "A"};
+
+            if (RegisterClass(&wc))
+            {
+                this->m_handle = CreateWindow("A", title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, nullptr, nullptr, nullptr, nullptr);
+
+                ShowWindow(this->m_handle, SW_SHOW);
+                UpdateWindow(this->m_handle);
+
+                this->m_isRunning = true;
+            }
+        }
+
+        // GETTERS
+        bool isRunning() const { return m_isRunning; }
+
+        // MESSAGE HANDLING
+        void update()
+        {
+            this->m_mouse.onUpdate();
+            
+            MSG msg;
+
+            while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+
+        LRESULT handleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
+        {
+            switch (msg)
+            {
+                case WM_MOVE:
+                    this->m_position  = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+                    return 0;
+                case WM_SIZE:
+                    this->m_dimension = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+                    return 0;
+                case WM_DESTROY:
+                    this->destroy();
+                    return 0;
+            }
+
+            if (this->m_mouse.handleMessage(msg, wParam, lParam)) return 0;
+            if (this->m_keyboard.handleMessage(msg, wParam, lParam)) return 0;
+
+            return DefWindowProc(this->m_handle, msg, wParam, lParam);
+        }
+
+        // DESTROYING
+        void destroy() { this->m_isRunning = !DestroyWindow(this->m_handle); }
+
+        ~Window() { this->destroy();
+    }
+};
+
+/*
 class Window
 {
 private:
@@ -155,59 +345,13 @@ public:
 
     LRESULT handleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
     {
-        switch (msg)
-        {
-        case WM_CREATE:
-            this->m_windowEvents.emplace_back(Event::WINDOW_CREATED);
-            break;
-        case WM_MOVE:
-            this->m_windowPosition = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
-            this->m_windowEvents.emplace_back(Event::WINDOW_MOVED);
-            break;
-        case WM_SIZE:
-            this->m_windowDimensions = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
-            this->m_windowEvents.emplace_back(Event::WINDOW_RESIZED);
-            break;
-        case WM_MOUSEMOVE:
-        {
-            this->m_mouseDeltaPosition = {
-                static_cast<int16_t>(static_cast<int16_t>(GET_X_LPARAM(lParam)) - static_cast<int16_t>(this->m_mousePosition[0])),
-                static_cast<int16_t>(static_cast<int16_t>(GET_Y_LPARAM(lParam)) - static_cast<int16_t>(this->m_mousePosition[1]))
-            };
-
-            this->m_mousePosition = {(uint16_t)GET_X_LPARAM(lParam), (uint16_t)GET_Y_LPARAM(lParam)};
-            this->m_windowEvents.emplace_back(Event::CURSOR_MOVED);
-        }
-        break;
-        case WM_LBUTTONDOWN:
-            this->m_isLeftMouseButtonDown = true;
-            break;
-        case WM_LBUTTONUP:
-            this->m_isLeftMouseButtonDown = false;
-            break;
-        case WM_RBUTTONDOWN:
-            this->m_isRightMouseButtonDown = true;
-            break;
-        case WM_RBUTTONUP:
-            this->m_isRightMouseButtonDown = false;
-            break;
-        case WM_MOUSEWHEEL:
-            this->m_mouseWheelDelta = GET_WHEEL_DELTA_WPARAM(wParam);
-            this->m_windowEvents.emplace_back(Event::MOUSE_WHEEL_ROTATING);
-            break;
-        case WM_DESTROY:
-            this->destroy();
-            this->m_windowEvents.emplace_back(Event::WINDOW_CLOSED);
-            break;
-        default:
-            return DefWindowProc(this->m_windowHwnd, msg, wParam, lParam);
-        }
+        
     }
 
     void destroy() { this->m_isRunning = !DestroyWindow(this->m_windowHwnd); }
 
     ~Window() { this->destroy(); }
-};
+};*/
 
 namespace WindowHandler
 {
@@ -224,7 +368,7 @@ namespace WindowHandler
 LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     std::vector<Window>::iterator it = std::find_if(WindowHandler::windows.begin(), WindowHandler::windows.end(), [&hwnd](const Window &window) {
-        return window.getHWND() == hwnd;
+        return window.m_handle == hwnd;
     });
 
     if (it != WindowHandler::windows.end())
