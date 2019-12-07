2bc6d20544c1f1ced7583fe67362ceb96eb44b73
PolarToCartesian <hleberre64@gmail.com>
2019-12-07 19:56:59 +0100

    Fixed Incomplete D3D11_SAMPLER_DESC

--- images/TextureSampler.cpp
+++ images/TextureSampler.cpp
@@ -4,10 +4,13 @@ TextureSampler::TextureSampler(const Microsoft::WRL::ComPtr<ID3D11Device>& pDevi
 : m_pDeviceContextRef(pDeviceContextRef), m_descriptor(descriptor)
 {
 	D3D11_SAMPLER_DESC samplerDescriptor;
-	samplerDescriptor.Filter = descriptor.filter;
-	samplerDescriptor.AddressU = descriptor.addressMode;
-	samplerDescriptor.AddressV = descriptor.addressMode;
-	samplerDescriptor.AddressW = descriptor.addressMode;
+	samplerDescriptor.Filter         = descriptor.filter;
+	samplerDescriptor.AddressU       = descriptor.addressMode;
+	samplerDescriptor.AddressV       = descriptor.addressMode;
+	samplerDescriptor.AddressW       = descriptor.addressMode;
+	samplerDescriptor.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	samplerDescriptor.MinLOD         = 0;
+	samplerDescriptor.MaxLOD         = D3D11_FLOAT32_MAX;
 
 	pDeviceRef->CreateSamplerState(&samplerDescriptor, &this->m_pSamplerState);
 }
