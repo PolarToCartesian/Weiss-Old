991808d96ad3080f2c676231fb089dfe32b11ae3
PolarToCartesian <hleberre64@gmail.com>
2020-01-26 19:03:45 +0100

    Texture Arrays

--- media/Texture2D.h
+++ media/Texture2D.h
@@ -6,7 +6,7 @@ class Texture2DCreationException : public std::exception { };
 struct Texture2DDescriptor
 {
 	ShaderBindingType bindingType;
-	Image image;
+	std::vector<Image> images;
 	UINT slotVS;
 	UINT slotPS;
 	bool useMipMaps;
@@ -23,18 +23,24 @@ public:
 	Texture2D(const Microsoft::WRL::ComPtr<ID3D11Device>& pDeviceRef, Microsoft::WRL::ComPtr<ID3D11DeviceContext>& pDeviceContextRef, const Texture2DDescriptor& descriptor)
 		: m_pDeviceContextRef(pDeviceContextRef), m_descriptor(descriptor)
 	{
-		D3D11_SUBRESOURCE_DATA subResourceData;
-		subResourceData.pSysMem = static_cast<void*>(descriptor.image.GetBuffer());
-		subResourceData.SysMemPitch = this->m_descriptor.image.GetWidth() * sizeof(Coloru8);
-		subResourceData.SysMemSlicePitch = 0;
+		D3D11_SUBRESOURCE_DATA* subResourceDataArr = new D3D11_SUBRESOURCE_DATA[this->m_descriptor.images.size()];
+
+		for (size_t i = 0u; i < this->m_descriptor.images.size(); i++) {
+			const Image* image = descriptor.images.data() + i;
+			D3D11_SUBRESOURCE_DATA& subResourceData = subResourceDataArr[i];
+
+			subResourceData.pSysMem = static_cast<void*>(image->GetBuffer());
+			subResourceData.SysMemPitch = image->GetWidth() * sizeof(Coloru8);
+			subResourceData.SysMemSlicePitch = 0;
+		}
 
 		D3D11_TEXTURE2D_DESC texture2DDescriptor = {};
-		texture2DDescriptor.Width = this->m_descriptor.image.GetWidth();
-		texture2DDescriptor.Height = this->m_descriptor.image.GetHeight();
+		texture2DDescriptor.Width = this->m_descriptor.images[0].GetWidth();
+		texture2DDescriptor.Height = this->m_descriptor.images[0].GetHeight();
 		texture2DDescriptor.MipLevels = this->m_descriptor.useMipMaps ? 0 : 1;
-		texture2DDescriptor.ArraySize = 1;
+		texture2DDescriptor.ArraySize = this->m_descriptor.images.size();
 		texture2DDescriptor.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		texture2DDescriptor.SampleDesc.Count = 1;
+		texture2DDescriptor.SampleDesc.Count = this->m_descriptor.images.size();
 		texture2DDescriptor.SampleDesc.Quality = 0;
 		texture2DDescriptor.Usage = D3D11_USAGE_DEFAULT;
 		texture2DDescriptor.BindFlags = this->m_descriptor.useMipMaps ? D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET : D3D11_BIND_SHADER_RESOURCE;
@@ -42,7 +48,7 @@ public:
 
 		Microsoft::WRL::ComPtr<ID3D11Texture2D> texture2D;
 
-		if (pDeviceRef->CreateTexture2D(&texture2DDescriptor, this->m_descriptor.useMipMaps ? nullptr : &subResourceData, &texture2D) != S_OK)
+		if (pDeviceRef->CreateTexture2D(&texture2DDescriptor, this->m_descriptor.useMipMaps ? nullptr : subResourceDataArr, &texture2D) != S_OK)
 		{
 #ifdef __WEISS_SHOW_DEBUG_ERRORS
 			MESSAGE_BOX_ERROR("Could Not Create Texture 2D");
@@ -52,7 +58,8 @@ public:
 		}
 
 		if (this->m_descriptor.useMipMaps)
-			this->m_pDeviceContextRef->UpdateSubresource(texture2D.Get(), 0u, nullptr, subResourceData.pSysMem, subResourceData.SysMemPitch, 0u);
+			for (size_t i = 0u; i < this->m_descriptor.images.size(); i++)
+				this->m_pDeviceContextRef->UpdateSubresource(texture2D.Get(), i, nullptr, subResourceDataArr[i].pSysMem, subResourceDataArr[i].SysMemPitch, 0u);
 
 		D3D11_SHADER_RESOURCE_VIEW_DESC SRVDescriptor = {};
 		SRVDescriptor.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
@@ -76,9 +83,9 @@ public:
 	void Bind() const noexcept
 	{
 		if (this->m_descriptor.bindingType == ShaderBindingType::VERTEX || this->m_descriptor.bindingType == ShaderBindingType::BOTH)
-			this->m_pDeviceContextRef->VSSetShaderResources(this->m_descriptor.slotVS, 1u, this->m_pShaderResourceView.GetAddressOf());
+			this->m_pDeviceContextRef->VSSetShaderResources(this->m_descriptor.slotVS, this->m_descriptor.images.size(), this->m_pShaderResourceView.GetAddressOf());
 
 		if (this->m_descriptor.bindingType == ShaderBindingType::PIXEL || this->m_descriptor.bindingType == ShaderBindingType::BOTH)
-			this->m_pDeviceContextRef->PSSetShaderResources(this->m_descriptor.slotPS, 1u, this->m_pShaderResourceView.GetAddressOf());
+			this->m_pDeviceContextRef->PSSetShaderResources(this->m_descriptor.slotPS, this->m_descriptor.images.size(), this->m_pShaderResourceView.GetAddressOf());
 	}
 };
--- timers/Timer.h
+++ timers/Timer.h
@@ -5,14 +5,14 @@
 class Timer
 {
 private:
-	std::chrono::system_clock::time_point m_start;
+	std::chrono::high_resolution_clock::time_point m_start;
 
 public:
-	Timer() { this->m_start = std::chrono::system_clock::now(); }
+	Timer() { this->m_start = std::chrono::high_resolution_clock::now(); }
 
 	[[nodiscard]] float GetElapsedTimeMs()
 	{
-		const auto end = std::chrono::system_clock::now();
+		const auto end = std::chrono::high_resolution_clock::now();
 		const auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - this->m_start);
 
 		return static_cast<float>(elapsed.count() * 0.001f);
